9d395dda761916963d4f3e24ad1c883dc17562b5
Danchik <124835518+TraktoristRuS54age@users.noreply.github.com>
2025-01-05 23:05:23 +0300

    Create GrammarReader.h

---
+++ GrammarReader/src/GrammarReader.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <regex>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <sstream>
+#include <stdexcept>
+#include <codecvt>
+
+using namespace std;
