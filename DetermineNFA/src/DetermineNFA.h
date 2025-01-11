441f38e186a9dec8b0f8eec1b71106c0be1179dc
Danchik <124835518+TraktoristRuS54age@users.noreply.github.com>
2025-01-12 01:07:18 +0300

    Create DetermineNFA.h

---
+++ DetermineNFA/src/DetermineNFA.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
